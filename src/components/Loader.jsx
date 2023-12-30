368a1474365cebe4aa5cb283426fb9907d6fb7e8
Jaxque <jackyyotsuba@gmail.com>
2023-12-29 20:50:12 -0500

    edited play page, created loader.jsx

---
+++ src/components/Loader.jsx
@@ -0,0 +1,15 @@
+import { Html } from "@react-three/drei"
+
+const Loader = () => {
+  return (
+    <Html>
+        <div className="flex items-left justify-left">
+            <div className="w-50 h-100 border-blue-500 rounded-full"/>
+        </div>
+    </Html>
+
+    
+  )
+}
+
+export default Loader
--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,6 +1,26 @@
+import {Canvas, useFrame} from '@react-three/fiber'
+import { useRef } from 'react'
+import { Mesh } from 'three'
+import { Suspense } from 'react'
+import Loader from '../components/Loader'
+
+
 const Play = () => {
   return (
-    <div>Play</div>
+    <section className="w-full h-screen relative">
+      <div>Play</div>
+
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{near:0.1, far: 1000}}
+      >
+        <Suspense fallback={<Loader/>}>
+          
+        </Suspense>
+      </Canvas>  
+    
+    </section>
+    
   )
 }
 
