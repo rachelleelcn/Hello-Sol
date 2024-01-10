eec107e7f6ce44aa744af861a6389acadcf7b21c
rachelleelcn <rachelleelcn@gmail.com>
2024-01-10 00:20:58 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Hello-Sol/",
   plugins: [react()],
-  assetsInclude: ['**/*.glb'], 
-  base: "/Hello-Sol/"
+  assetsInclude: ['**/*.glb']
 })
