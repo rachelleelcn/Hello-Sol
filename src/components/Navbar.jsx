448b8c3c6356e367a6ae7b5f065648f095501c63
rachelleelcn <rachelleelcn@gmail.com>
2024-01-10 02:55:44 -0500

    update nav for deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,20 +14,20 @@ const Navbar = () => {
     
     return (
         <header className='header'>
-            <NavLink to='/learn' onClick={() => (isActive('/learn') ? handleClick() : null)}>
+            <NavLink to='/Hello-Sol/learn' onClick={() => (isActive('/Hello-Sol/learn') ? handleClick() : null)}>
                 <div className="text-xl font-bold font-inter">Hello-Sol</div>
             </NavLink>
             <nav className='items-center justify-center flex gap-8'>
-                <NavLink to='/learn' className={({ isActive }) => isActive ? "underline underline-offset-4" : null } onClick={() => (isActive('/learn') ? handleClick() : null)}>
+                <NavLink to='/Hello-Sol/learn' className={({ isActive }) => isActive ? "underline underline-offset-4" : null } onClick={() => (isActive('/Hello-Sol/learn') ? handleClick() : null)}>
                     <div className="text-sm font-inter">Learn</div>
                 </NavLink>
-                <NavLink to='/create' className={({ isActive }) => isActive ? "underline underline-offset-4" : null } onClick={() => (isActive('/create') ? handleClick() : null)}>
+                <NavLink to='/Hello-Sol/create' className={({ isActive }) => isActive ? "underline underline-offset-4" : null } onClick={() => (isActive('/Hello-Sol/create') ? handleClick() : null)}>
                     <div className="text-sm font-inter">Create</div>
                 </NavLink>
-                <NavLink to='/play' className='rounded-full bg-black-200 items-center justify-center flex' onClick={() => (isActive('/play') ? handleClick() : null)}>
+                <NavLink to='/Hello-Sol/play' className='rounded-full bg-black-200 items-center justify-center flex' onClick={() => (isActive('/Hello-Sol/play') ? handleClick() : null)}>
                     <div className="text-white-100 text-sm font-inter py-2 px-6">Play</div>
                 </NavLink>
-                <NavLink to='/geo' onClick={() => (isActive('/geo') ? handleClick() : null)}>
+                <NavLink to='/Hello-Sol/geo' onClick={() => (isActive('/Hello-Sol/geo') ? handleClick() : null)}>
                     <img src={geo_icon} alt='geo-logo' className='w-10 object-contain mt-1.5' />
                 </NavLink>
             </nav>
