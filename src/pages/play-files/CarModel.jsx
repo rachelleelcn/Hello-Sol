eed1b09524edf697c825dbb8ef60518557e66dd4
Jaxque <jackyyotsuba@gmail.com>
2024-01-17 18:01:49 -0500

    fixed file name

--- src/pages/play-files/CarModel.jsx
+++ src/pages/play-files/CarModel.jsx
@@ -9,7 +9,7 @@ import { OrbitControls } from '@react-three/drei'
 
 import { useWheels } from './useWheels'
 import { WheelDebug } from './WheelDebug'
-import { UseControls } from './useControls'
+import { UseControls } from './UseControls'
 
 export function CarModel() {
     const ref = useRef()
